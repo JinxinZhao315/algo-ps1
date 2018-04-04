c6b3ee193797973750b9e8213120ded8c0f439cb
Zhao Jinxin <jinxinzhao315@gmail.com>
2018-04-04 16:57:37 +0800

    ex5

--- ex5.py
+++ ex5.py
@@ -1,1 +1,19 @@
 # Type your code for insertion sort (Exercise 5) here
+numbers_str = input('Type a list of numbers you want to sort, seperated by comma: ')
+numbers_list = []
+
+
+for char in numbers_str.split(','):
+    numbers_list.append(int(char))
+
+
+for j in range(1,len(numbers_list)):
+    key = numbers_list[j]
+    i = j - 1
+    while i >= 0 and numbers_list[i]>key:
+        numbers_list[i+1] = numbers_list[i]
+        i -= 1
+    numbers_list[i+1] = key
+
+print(numbers_list)
+
